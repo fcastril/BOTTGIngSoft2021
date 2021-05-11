4dddeb5ea8e8aa7d7051cdb71ec8f6a2a8bf93e0
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2021-05-10 21:01:27 -0500

    Add API Project and First Entity (Intent), And Change Configuration Azure

---
+++ BOTTGIngSoft2021.Data/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTTGIngSoft2021.Data.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ BOTTGIngSoft2021.Data/Entities/Intent.cs
@@ -0,0 +1,9 @@
+﻿namespace BOTTGIngSoft2021.Data.Entities
+{
+    public class Intent : BaseEntity
+    {
+        public string Name { get; set; }
+        public int TypeId { get; set; }
+        public string ReadableType { get; set; }
+    }
+}
